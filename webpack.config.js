8cd9d58cc3463821cc8a41f963e54f626810f4c5
keisokoo <keisokoo@gmail.com>
2024-10-20 00:37:24 +0900

    feat, dev reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,63 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const ExtReloader = require("webpack-ext-reloader");
 
-module.exports = {
-  entry: {
-    index: "./src/index.tsx",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "manifest.json", to: "manifest.json" },
-        { from: "background.js", to: "background.js" },
-        { from: "public/icon16.png", to: "icon16.png" },
-        { from: "public/icon48.png", to: "icon48.png" },
-        { from: "public/icon128.png", to: "icon128.png" },
-      ],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: {
+      index: "./src/index.tsx",
+      background: "./background.js",
     },
-    compress: true,
-    port: 3000,
-  },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+        chunks: ["index"],
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "manifest.json", to: "manifest.json" },
+          { from: "public/icon16.png", to: "icon16.png" },
+          { from: "public/icon48.png", to: "icon48.png" },
+          { from: "public/icon128.png", to: "icon128.png" },
+        ],
+      }),
+      ...(isProduction
+        ? []
+        : [
+            new ExtReloader({
+              port: 8012,
+              reloadPage: true,
+              entries: {
+                extensionPage: "index",
+                background: "background",
+              },
+            }),
+          ]),
+    ],
+    devtool: isProduction ? false : "inline-source-map",
+  };
 };
